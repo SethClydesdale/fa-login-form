86d86c314fe11af8fd21103e3a0788b71f8be6f9
Seth Clydesdale <sethc1995@gmail.com>
2016-04-26 20:53:21 -0400

    Add fallback
    Fallback to the classic login if the custom login cannot be reached.

--- redirecter.js
+++ redirecter.js
@@ -2,15 +2,24 @@
   var html_page = '{HTML_PAGE_LINK}',
       link_change = true,
       redirect = true;
-  
+ 
   // redirect
   if (redirect && /\/login/.test(window.location.href) && !/login_classic/.test(window.location.hash)) window.location.href = html_page;
-  
-  // link change
-  link_change && !new RegExp(html_page).test(window.location.href) && $(function() {
-    $('a[href^="/login"], a[href^="http://' + window.location.host + '/login"]').attr('href', html_page);
+ 
+  $(function() {
+    var regex = new RegExp(html_page);
+   
+    // link change
+    if (link_change && !regex.test(window.location.href)) {
+      $('a[href^="/login"], a[href^="http://' + window.location.host + '/login"]').attr('href', html_page);
+    }
+ 
+    // redirect to classic if login page isn't available
+    if (!document.getElementById('fa_form_container') && regex.test(window.location.href)) {
+      window.location.href = '/login#login_classic';
+    }
   });
-  
+ 
   // login redirection
   // saves redirect location so you're taken to the correct page upon login
   if (/\/login\?redirect/.test(window.location.href)) {
